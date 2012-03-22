9f2d472f4cc39010a5347adb1860b5683a69ef1f
jaskeen <jaskeen@gmail.com>
2012-03-22 09:03:11 -0600

    Revert "DataPractice Added"
    This reverts commit ed5383648d2d7984aaedda631a459426fe412184.

--- DataPractice/formOutput.php
+++
@@ -1,9 +0,0 @@
-<?php
-$new = "hello";
-extract($_GET);
-//extract($_POST);
-
-echo "This means that when the sky is $fColor, you will gain $fNumber years of life.";
-?>
-
-
--- DataPractice/getJSON.lua
+++
@@ -1,8 +0,0 @@
-require "json"
-local t= json.decode(jsonFile("jsonPractice.json"))
-
-local value = json.encode (t)
- 	print (value)  --> {"name1":"value1","name3":null,"name2":[1,false,true,23.54,"a \u0015 string"]}
- 
-local t = json.decode(value)
-	print(t.name2[4])  --> 23.54
