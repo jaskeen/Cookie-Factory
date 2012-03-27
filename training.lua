f763760c03e58944af6589ef8e846b18cb8da0d3
Peter <pjrich@gmail.com>
2012-03-26 21:48:57 -0600

    started to integrate spawn into training

--- training.lua
+++ training.lua
@@ -37,24 +37,44 @@ function onBtnRelease(event)
 	return true	-- indicates successful touch
 end
 	
--- Called when the scene's view does not exist:
+---------------------  CREATE SCENE: Called when the scene's view does not exist: ---------------------
+	--	CREATE display objects and add them to 'group' here.
 function scene:createScene( event ) 
 	local group = self.view  --insert all display objects into this
+	group:insert(cloud)
+	group:insert(showValue)
 	
 	local bg = display.newImageRect("images/factoryBG.png",1024,768)
 	bg.x = _W/2; bg.y = _H/2
+	group:insert(bg)
 	--local conveyorBelt = display.newImageRect("images/conveyorSprite.png",930, 190)
 	--conveyorBelt:setReferencePoint(display.TopLeftReferencePoint)
 	--conveyorBelt.x = 10; conveyorBelt.y = 70;
 	local ipad = display.newImageRect("images/iPad.png",250, 200)
 	ipad:setReferencePoint(display.TopLeftReferencePoint);
 	ipad.x = 30; ipad.y = 550
-
-	factoryBG= display.newImageRect("images/factoryBG.png", _W, _H)
-	factoryBG:setReferencePoint(display.CenterReferencePoint)
-	factoryBG.x = _W/2
-	factoryBG.y = _H/2
-	factoryBG.scene="menu"
+	group:insert(ipad)
+	---------------------------------------- Number BOXES ----------------------------------------
+	local tenThousandsTray = display.newRect(0,0, 130, 40)
+	tenThousandsTray:setFillColor(175)
+	local thousandsTray = display.newRect(130,0, 130, 40)
+	thousandsTray:setFillColor(100)
+	local hundredsTray = display.newRect(260, 0, 130, 40)
+	hundredsTray:setFillColor(200)
+	local tensTray = display.newRect(390, 0, 130, 40)
+	tensTray:setFillColor(150)
+	local onesTray = display.newRect(520, 0, 130, 40)
+	onesTray:setFillColor(255)
+	--now put them all into a group so you can move the group around with ease
+	local trayGroup = display.newGroup()
+	trayGroup:insert(tenThousandsTray)
+	trayGroup:insert(thousandsTray)
+	trayGroup:insert(hundredsTray)
+	trayGroup:insert(tensTray)
+	trayGroup:insert(onesTray)
+	trayGroup.x = 298; trayGroup.y = 645
+	group:insert(trayGroup)
+	
 	
 	homeBtn=widget.newButton{
 		default="images/homeBtn.png",
@@ -66,22 +86,50 @@ function scene:createScene( event )
 	homeBtn.x = _W*.06
 	homeBtn.y = _H*.07
 	homeBtn.scene="menu"
-	
-
-	local trainingTitle=display.newImageRect("images/trainingMode.png", 300, 60)
-	trainingTitle:setReferencePoint( display.CenterReferencePoint )
-	trainingTitle.x = _W/2 
-	trainingTitle.y = _H/2 
-
-	-----------------------------------------------------------------------------
-		
-	--	CREATE display objects and add them to 'group' here.
-	--	Example use-case: Restore 'group' from previously saved state.
-	
-	-----------------------------------------------------------------------------
-	group:insert(factoryBG)
-	group:insert(trainingTitle)
 	group:insert(homeBtn)
+
+	
+	--instead of creating an object for each cookie, let's only create objects that we'll actually use during this "round"
+	local themes= {"creme", "pb","jelly","chocchip"}
+	local theme = themes[level]
+	local shapes = {}
+	shapes[10] = {-30, -45,   8, -45,   25, 37,   23, 47,   -8, 47,   -30, -31}
+	shapes[100] = {-40, -36,   28, -36,   40, -30,   40, 36,   -30, 36,   -40, 26}
+	shapes[1000] = {-84, -54,   33, -54,   83, -8,   90, 48,   90, 54,   -40, 54,   -90, 6 }
+	shapes[10000] = {-90,-90,   34,-96,   91,-58,   90,87,   -54, 94,    -90,52}
+	local items = {}
+	createItemsForThisLevel = function(theme)
+		items[1] = {name = theme, value = 1, w=48, h=48, units = 1, radius=24}
+		items[10] = {name=theme, value = 10, w=80, h=95, units=10, radius=0, shape= shapes[10]}
+		items[100] = {name=theme, value = 100, w=80,h=71, units=100, radius=0, shape=shapes[100]}
+		items[1000] = {name=theme,value=1000, w=180,h=108, units=1000, radius=0, shape= shapes[1000]}
+		items[10000] = {name=theme,value = 10000, w=180,h= 189, units=10000, radius=0, shape = shapes[10000] }
+		for i=9,1.9, -1 do 
+			items[i] = {name=theme, value = 10, w=80, h=95, units=1, radius=0, shape=shapes[10]}
+		end
+		for i=90,19,-10 do 
+			items[i] = {name=theme, value = 100, w=80,h=71, units=10, radius=0, shape=shapes[100]}
+		end
+		for i=900, 199, -100 do 
+			items[i] = {name=theme,value=1000, w=180,h=108, units=100, radius=0, shape=shapes[1000]}
+		end
+		for i=9000,1999, -1000 do 
+			items[i] = {name=theme,value = 10000, w=180,h= 189, units=1000, radius=0, shape = shapes[10000]}
+		end
+	end
+	createItemsForThisLevel(theme)
+	
+	--list which cookies are available on each level
+	local levelObjects = {
+		{items[1], items[1], items[1], items[10]},
+		{items[1], items[10],items[1], items[1], items[100]},
+		{items[1], items[10],items[100], items[1], items[1], items[1000]},
+		{items[1], items[10],items[100], items[1], items[10], items[1], items[1000], items[10000]},
+	}
+	thisLevel = levelObjects[level]
+		
+--I STOPPED RIGHT HERE ---
+	
 	
 end
 
