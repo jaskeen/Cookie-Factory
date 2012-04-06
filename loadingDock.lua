ad761e92bf3151d69dbf6afe627d5cb4c274b779
jaskeen <jaskeen@gmail.com>
2012-04-06 17:04:34 -0600

    loadingDock progress

--- loadingDock.lua
+++ loadingDock.lua
@@ -1,8 +1,6 @@
 ----------------------------------------------------------------------------------
---
--- scenetemplate.lua
---
-----------------------------------------------------------------------------------
+---- loadingDock.lua
+------------------------------------------------------------------------------------
 local generate = require "generateNumInfo"
 local storyboard = require "storyboard" 
 local widget= require "widget"
@@ -14,34 +12,16 @@ physics.setGravity(0,0)
 _H = display.contentHeight
 _W = display.contentWidth
 
---number of cookies
---[[c = 0
-totalCookies=3
-ready=false
-]]
-
 --Variables and Functions
-
 local onBtnRelease
-local factoryBG
-local homeBtn
-local truck1 
-local truck2
-local truck3
-local truck4
-local object1
-local object2
-local object3
-local loadingDockTitle
-local truckNum
-local truckNum2
-local truckNum3
-local truckNum4
-local cookie
-local spawnCookies
-local cookie1
-local cookie2
-local cookie3
+local factoryBG, homeBtn,truck1,truck2,truck3,truck4,object1,object2,object3
+local loadingDockTitle,truckNum,truckNum2,truckNum3,truckNum4,cookie,spawnCookies
+local cookie1,cookie2,cookie3
+local myObject
+local startDrag
+local level
+	--From generateNumInfo
+	local newList
 
 
 
@@ -59,6 +39,8 @@ local cookie3
 ---------------------------------------------------------------------------------
 
 -- Called when the scene's view does not exist:
+level=5	
+
 -- 'onRelease' event listener for return to main menu
 function onBtnRelease(event)
 	
@@ -68,104 +50,108 @@ function onBtnRelease(event)
 	return true	-- indicates successful touch
 end
 	
-----Spawn Cookies Function
-	
---[[function spawnCookies(name, value,w,h, units, radius, shape,x,y)
-	image = ("images/"..name..value..".png")
-	cookie = display.newImageRect(image, w,h)
-	cookie: setReferencePoint(display.CenterReferencePoint)
-	cookie.name = name
-	cookie.value = value or 1
-	cookie.x=100
-	cookie.y=math.random(100,_H-200)
-	cookie.units = units
-	physics.addBody(cookie, {radius=radius, shape=shape})
-	cookie.isFixedRotation = true
-	cookie.linearDamping = 9
-	
-	--every time a cookie is spawn, increment c by 1
-	c = c+1
-	
-	--track the number of cookies spawn(c)
-	if(c == 3)then
-		ready=true
-	else
-		ready=false
-	end
-	
-	cookie:addEventListener("touch", cookie)
-	return cookie
-	
-end
-]]
 
+
+
+--Traffic light with the #of cookies as red circles
+	--when one correct match is made, one of the red circles becomes green
+	--when all of the circles are green, the timer stops and the game is finished
+--Timer: the amount of time it takes to get all of the trucks loaded correctly
+	
 function scene:createScene( event )
 	local group = self.view
 
 	
-	---create 3 COOKIES----------------------------------------- 
-	--PROBLEM: they are stuck... is it because of how I have them grouped? 
-	cookie1=display.newImageRect("images/chocchip3.png", 150, 150)
+	
+--DRAG FUNCTION	
+function startDrag( event )
+	local t = event.target
+
+	local phase = event.phase
+	if "began" == phase then
+		display.getCurrentStage():setFocus( t )
+		t.isFocus = true
+
+		-- Store initial position
+		t.x0 = event.x - t.x
+		t.y0 = event.y - t.y
+		
+		-- Make body type temporarily "kinematic" (to avoid gravitional forces)
+		event.target.bodyType = "kinematic"
+		event.target.isFixedRotation= true
+		
+	elseif t.isFocus then
+		if "moved" == phase then
+			t.x = event.x - t.x0
+			t.y = event.y - t.y0
+
+		elseif "ended" == phase or "cancelled" == phase then
+			display.getCurrentStage():setFocus( nil )
+			t.isFocus = false
+			
+			-- Switch body type back to "dynamic", unless we've marked this sprite as a platform
+			if ( not event.target.isPlatform ) then
+				event.target.bodyType = "dynamic"
+			end
+		end
+	end
+	--end the touch event when ended
+	return true
+end
+
+--Spawn cookies, which should be the #trucks-1
+	--the omitted# from the trucks (with the exception of 1) will determine the cookie to appear
+	
+--Create the Cookies
+	cookie1=display.newText("Value#1",  (_W/2)-50, 275, native.systemFontBold, 30)
 	physics.addBody( cookie1, "kinematic", { friction=0.7 } )
-		cookie1.x=150
+				cookie1.x=150
 		cookie1.y=200
-	cookie2=display.newImageRect("images/chocchip300.png", 150, 150)
+	cookie2=display.newText("Value#2",  (_W/2)-50, 275, native.systemFontBold, 30)
 	physics.addBody( cookie2, "kinematic", { friction=0.7 } )
 		cookie2.x=150
 		cookie2.y=400
-	cookie3=display.newImageRect("images/chocchip30.png", 150, 150)
+	cookie3=display.newText("Value#3",  (_W/2)-50, 275, native.systemFontBold, 30)
 	physics.addBody( cookie3, "kinematic", { friction=0.7 } )
 		cookie3.x=150
 		cookie3.y=600
-		
-	function cookieTouch(event)
-		if(event.phase=="began")then
-			print("began")
-		elseif(event.phase=="moved")then
-			print("moved")
-		elseif(event.phase=="ended")then
-			print("ended")
-		
-		end
-		
-		return true	
-	end
-	
-	cookie1:addEventListener("touch", cookieTouch)
-	cookie2:addEventListener("touch", cookieTouch)
-	cookie3:addEventListener("touch", cookieTouch)
+
+
+-- Add touch event listeners to objects
+	--make cookies DRAGGABLE
+cookie1:addEventListener( "touch", startDrag )
+cookie2:addEventListener( "touch", startDrag )
+cookie3:addEventListener( "touch", startDrag )
 	
 	
-	--[[cookieGroup = display.newGroup()
-		cookieGroup:insert(cookie1)
-		cookieGroup:insert(cookie2)
-		cookieGroup:insert(cookie3)
-	]]
-			
+	
 	----Create TRUCKS--------------------------------
+--Spawn trucks, which should either be a constant 4 trucks OR the level+2 trucks
+	--each truck will have a random number with one ommitted# displayed on it
+	--make trucks SENSORS
 	
 	truck1 = display.newImageRect( "images/truckTemp.png", 200, 150  )
 	truck1.x = _W/2
 	truck1.y = 85
-	physics.addBody( truck1, "kinematic", { friction=0.7 } )
+	physics.addBody( truck1, "kinematic", { isSensor=true } )
 	--truck1.isPlatform = true -- custom flag, used in drag function above
 	
 	truck2= display.newImageRect( "images/truckTemp.png", 200, 150 )
 	truck2.x = _W/2
 	truck2.y = 285
-	physics.addBody( truck2, "kinematic", { friction=0.7 } )
+	physics.addBody( truck2, "kinematic", { isSensor=true } )
 	--truck2.isPlatform = true -- custom flag, used in drag function above
 	
 	truck3 = display.newImageRect( "images/truckTemp.png", 200, 150 )
 	truck3.x = _W/2
 	truck3.y = 485
-	physics.addBody( truck3, "kinematic", { friction=0.7 } )
+	physics.addBody( truck3, "kinematic", { isSensor=true } )
 	--truck3.isPlatform = true -- custom flag, used in drag function above
 	
 	truck4 = display.newImageRect( "images/truckTemp.png", 200, 150 )
 	truck4.x = _W/2
 	truck4.y = 675
-	physics.addBody( truck4, "kinematic", { friction=0.7 } )
+	physics.addBody( truck4, "kinematic", { isSensor=true } )
 	--truck4.isPlatform = true -- custom flag, used in drag function above
 
 	function truckNum()
@@ -174,19 +160,19 @@ function scene:createScene( event )
 		
 	end
 	
-	truckNum= display.newText("truckNum",  (_W/2)-50, 75, native.systemFontBold, 20)
+	truckNum= display.newText("Omitted#1",  (_W/2)-50, 75, native.systemFontBold, 20)
 			--I want truckNum from the function to be the text, not "truckNum"
 			--so it would be 4 random numbers concatinated
 			--truckNum".."truckNum".."truckNum".."truckNum".."truckNum
 			truckNum:setTextColor(255, 255, 255)
 			
-	truckNum2= display.newText("truckNum",  (_W/2)-50, 275, native.systemFontBold, 20)
+	truckNum2= display.newText("Omitted#2",  (_W/2)-50, 275, native.systemFontBold, 20)
 			truckNum:setTextColor(255, 255, 255)
 	
-	truckNum3= display.newText("truckNum",  (_W/2)-50, 475, native.systemFontBold, 20)
+	truckNum3= display.newText("Omitted#3",  (_W/2)-50, 475, native.systemFontBold, 20)
 			truckNum:setTextColor(255, 255, 255)
 			
-	truckNum4= display.newText("truckNum",  (_W/2)-50, 675, native.systemFontBold, 20)
+	truckNum4= display.newText("Omitted#4",  (_W/2)-50, 675, native.systemFontBold, 20)
 			truckNum:setTextColor(255, 255, 255)
 ----Create a truck group so they can move together-------------------------
 	
@@ -221,7 +207,7 @@ function scene:createScene( event )
 	homeBtn.y = _H*.07
 	homeBtn.scene="menu"
 	
-	loadingDockTitle=display.newImageRect("images/supervisorMode.png", 300, 60)
+	loadingDockTitle=display.newText("Loading Dock",  (_W/2)-50, 275, native.systemFontBold, 40)
 	loadingDockTitle:setReferencePoint( display.CenterReferencePoint )
 	loadingDockTitle.x = _W/2 
 	loadingDockTitle.y = _H/2 
@@ -247,11 +233,8 @@ function scene:createScene( event )
 	
 end
 
---[[Runtime:addEventListener("touch", cookie1 )
-Runtime:addEventListener("touch", cookie2)	
-Runtime:addEventListener("touch", cookie3)
-	]]
 -- Add touch event listeners to objects
+
 
 -- Called immediately after scene has moved onscreen:
 function scene:enterScene( event )
